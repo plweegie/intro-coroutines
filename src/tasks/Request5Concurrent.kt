710826d1b98b1bcb2f1e26a7499fd143e680cd25
Jan K. Szymanski <Jan.Szymanski@dyson.com>
2019-12-08 17:09:38 +0000

    Showing progress

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -10,8 +10,9 @@ suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData)
         .body() ?: listOf()
 
     val deferreds: List<Deferred<List<User>>> = repos.map { repo ->
-        async(Dispatchers.Default) {
+        async {
             log("starting loading for ${repo.name}")
+            delay(3000)
             service
                 .getRepoContributors(req.org, repo.name)
                 .also { logUsers(repo, it) }
