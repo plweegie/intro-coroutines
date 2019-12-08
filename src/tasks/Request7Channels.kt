b0e75c1d05910dfbf111541743ee504c86190a72
Jan K. Szymanski <Jan.Szymanski@dyson.com>
2019-12-08 17:21:32 +0000

    Use channels

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -8,9 +8,27 @@ import kotlinx.coroutines.launch
 suspend fun loadContributorsChannels(
     service: GitHubService,
     req: RequestData,
-    updateResults: suspend (List<User>, completed: Boolean) -> Unit
-) {
-    coroutineScope {
-        TODO()
+    updateResults: suspend (List<User>, completed: Boolean) -> Unit) = coroutineScope {
+
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    val channel = Channel<List<User>>()
+    repos.forEach { repo ->
+        launch {
+            val users = service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+            channel.send(users)
+        }
+    }
+
+    var allUsers = emptyList<User>()
+    repeat(repos.size) {
+        val users = channel.receive()
+        allUsers = (allUsers + users).aggregate()
+        updateResults(allUsers, it == repos.lastIndex)
     }
 }
